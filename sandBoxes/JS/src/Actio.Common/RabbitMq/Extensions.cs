54648afc72f3ca4ae5a120a54e4fd69eff91550e
James Sparham <jsparham777@gmail.com>
2018-11-13 16:43:38 +0000

    Added comments to helpers and extension methods

--- sandBoxes/JS/src/Actio.Common/RabbitMq/Extensions.cs
+++ sandBoxes/JS/src/Actio.Common/RabbitMq/Extensions.cs
@@ -7,20 +7,42 @@ using System.Threading.Tasks;
 
 namespace Actio.Common.RabbitMq
 {
+    /// <summary>
+    /// Helper Extension methods
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TCommand"></typeparam>
+        /// <param name="bus"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
         public static Task WithCommandHandlerAsync<TCommand>(this IBusClient bus,
             ICommandHandler<TCommand> handler) where TCommand : ICommand
             => bus.SubscribeAsync<TCommand>(msg => handler.HandleAsync(msg),
                 ctx => ctx.UseConsumerConfiguration(cfg =>
                 cfg.FromDeclaredQueue(q => q.WithName(GetQueueName<TCommand>()))));
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEvent"></typeparam>
+        /// <param name="bus"></param>
+        /// <param name="handler"></param>
+        /// <returns></returns>
         public static Task WithEventHandlerAsync<TEvent>(this IBusClient bus,
             IEventHandler<TEvent> handler) where TEvent : IEvent
             => bus.SubscribeAsync<TEvent>(msg => handler.HandleAsync(msg),
                 ctx => ctx.UseConsumerConfiguration(cfg =>
                 cfg.FromDeclaredQueue(q => q.WithName(GetQueueName<TEvent>()))));
 
+        /// <summary>
+        /// Gets the queue name
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns>The name of the queue</returns>
         private static string GetQueueName<T>()
             => $"{Assembly.GetEntryAssembly().GetName()}/{typeof(T).Name}";
     }
--- sandBoxes/JS/src/Actio.Common/Services/ServiceHost.cs
+++ sandBoxes/JS/src/Actio.Common/Services/ServiceHost.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using RawRabbit;
+using System;
 
 namespace Actio.Common.Services
 {
@@ -12,68 +13,117 @@ namespace Actio.Common.Services
     {
         private readonly IWebHost _webHost;
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="webHost">The configured web host</param>
         public ServiceHost(IWebHost webHost)
         {
             _webHost = webHost;
         }
 
+        /// <summary>
+        /// Runs the configured web host
+        /// </summary>
         public void Run() => _webHost.Run();
 
-        public static HostingBuilder Create<TStartup>(string[] args) where TStartup : class
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TStartup"></typeparam>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static HostBuilder Create<TStartup>(string[] args) where TStartup : class
         {
+            //Set the console window title to the namespace of the type
             Console.Title = typeof(TStartup).Namespace;
+
+            //Create the configuration
             var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables()
-                .AddCommandLine(args)
-                .Build();
+                .AddEnvironmentVariables()  //Load the environment variables
+                .AddCommandLine(args)       //Load the command line variables
+                .Build();                   //Build the configuration
 
+            //Create the web host builder
             var webHostBuilder = WebHost.CreateDefaultBuilder(args)
-
                 .UseConfiguration(config)
                 .UseStartup<TStartup>();
 
+            //Return the host builder
             return new HostBuilder(webHostBuilder.Build());
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public abstract class BuilderBase
         {
             public abstract ServiceHost Build();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public class HostBuilder : BuilderBase
         {
             private readonly IWebHost _webHost;
             private IBusClient _bus;
-                
+               
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="webHost"></param>
             public HostBuilder(IWebHost webHost)
             {
                 _webHost = webHost;
             }
 
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <returns></returns>
             public BusBuilder UseRabbitMq()
             {
-                _bus = _webHost.Services.GetService(typeof(IBusClient));
+                _bus = (IBusClient)_webHost.Services.GetService(typeof(IBusClient));
 
                 return new BusBuilder(_webHost, _bus);
             }
 
+            /// <summary>
+            /// Builds the service host using the web host
+            /// </summary>
+            /// <returns></returns>
             public override ServiceHost Build()
             {
                 return new ServiceHost(_webHost);
             }
         }
 
+
+        /// <summary>
+        /// 
+        /// </summary>
         public class BusBuilder : BuilderBase
         {
             private readonly IWebHost _webHost;
             private IBusClient _bus;
 
+            /// <summary>
+            /// Constructor
+            /// </summary>
+            /// <param name="webHost"></param>
+            /// <param name="bus"></param>
             public BusBuilder(IWebHost webHost, IBusClient bus)
             {
                 _webHost = webHost;
                 _bus = bus;
             }
 
+            /// <summary>
+            /// Subscribes to commands
+            /// </summary>
+            /// <typeparam name="TCommand">The command to  subscribe to</typeparam>
+            /// <returns></returns>
             public BusBuilder SubscribeToCommand<TCommand>() where TCommand : ICommand
             {
                 var handler = (ICommandHandler<TCommand>)_webHost.Services
@@ -84,6 +134,11 @@ namespace Actio.Common.Services
                 return this;
             }
 
+            /// <summary>
+            /// Subscribes to events
+            /// </summary>
+            /// <typeparam name="TEvent">The event to subscribe to</typeparam>
+            /// <returns></returns>
             public BusBuilder SubscribeToEvent<TEvent>() where TEvent : IEvent
             {
                 var handler = (IEventHandler<TEvent>)_webHost.Services
@@ -94,6 +149,10 @@ namespace Actio.Common.Services
                 return this;
             }
 
+            /// <summary>
+            /// Builds the service host using the web host
+            /// </summary>
+            /// <returns></returns>
             public override ServiceHost Build()
             {
                 return new ServiceHost(_webHost);
