8281d2b10fda8823667b06f510f85a5a85d459dc
James Sparham <jsparham777@gmail.com>
2018-12-19 17:37:45 +0000

    Added a docker clean script

---
+++ sandBoxes/JS/scripts/Powershell/Docker clean.ps1
@@ -0,0 +1,12 @@
+﻿#Deleting all the containers
+#Before deleting the volumes, you need to delete all the existing containers using the following command (make sure the application is not running)
+
+docker rm $(docker ps -a -q) -f
+
+#Deleting all the volumes
+#Once all the containers are deleted, you can delete all the Docker volumes on your computer using the following command
+docker volume prune
+
+#If you don't want to delete all the Docker volumes on your computer, you can search for a specific one and deleting it
+#docker volume ls
+#docker volume rm <name_of_volume>
