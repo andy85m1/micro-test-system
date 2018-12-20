3dbc847271956e800d45b99cc72ce52ab05e214d
James Sparham <jsparham777@gmail.com>
2018-12-20 17:41:19 +0000

    Added unit tests for the API controllers

---
+++ sandBoxes/JS/Actio.Api.Tests/Unit/Controllers/ActivitiesControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Actio.Api.Controllers;
+using Actio.Api.Repositories;
+using Actio.Common.Commands;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RawRabbit;
+using System;
+using System.Security.Claims;
+using Xunit;
+
+namespace Actio.Api.Tests.Unit.Controllers
+{
+    public class ActivitiesControllerTests
+    {
+        [Fact]
+        public async void ActivitiesController_Post_ShouldReturnAccepted()
+        {
+            var busClientMock = new Mock<IBusClient>();
+            var activityRepositoryMock = new Mock<IActivityRepository>();
+            var controller = new ActivitiesController(busClientMock.Object, activityRepositoryMock.Object);
+
+            var userId = Guid.NewGuid();
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(
+                        new Claim[] { new Claim(ClaimTypes.Name, userId.ToString()) }
+                        , "test"))
+                }
+            };
+
+            var command = new CreateActivity
+            {
+                Id = Guid.NewGuid(),
+                UserId = userId
+            };
+
+            var result = await controller.Post(command);
+
+            var contentResult = result as AcceptedResult;
+            contentResult.Should().NotBeNull();
+            contentResult.Location.Should().BeEquivalentTo($"activities/{command.Id}");
+        }
+    }
+}
---
+++ sandBoxes/JS/Actio.Api.Tests/Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Actio.Api.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace Actio.Api.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void HomeController_Get_ShouldReturnStringContent()
+        {
+            //Arrange
+            var controller = new HomeController();
+
+            //Act
+            var result = controller.Get();            
+            var contentResult = result as ContentResult;
+
+            //Assert
+            contentResult.Should().NotBeNull();
+            contentResult.Content.Should().BeEquivalentTo("Hello from Actio API!");        }
+    }
+}
